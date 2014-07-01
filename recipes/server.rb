d4f8608cd98783e86941c1594b7153e86b273efe
Kyle House <kyle.house@khouse-mbpr.local>
2014-06-30 22:29:32 -0500

    update ssl certs, change template path

--- recipes/server.rb
+++ recipes/server.rb
@@ -10,34 +10,26 @@ template "/opt/logstash/patterns/token" do
   source "token_pattern.erb"
 end
 
-directory "/etc/logstash/keys" do
-  owner "logstash"
-  group "logstash"
-  mode "0600"
+directory "/etc/ssl/private" do
+  mode '0755'
 end
 
-remote_file "Copy cert chain" do
-  path "/etc/logstash/keys/DigiCertCA.crt"
-  source "file:///etc/ssl/certs/DigiCertCA.crt"
+file "/etc/ssl/certs/DigiCertCA.crt" do
   owner 'logstash'
   group 'logstash'
-  mode "0400"
+  mode '0400'
 end
 
-remote_file "Copy ssh cert" do
-  path "/etc/logstash/keys/#{node['service']['ssl_key_name']}.crt"
-  source "file:///etc/ssl/certs/#{node['service']['ssl_key_name']}.crt"
+file "/etc/ssl/certs/#{node['service']['ssl_key_name']}.crt" do
   owner 'logstash'
   group 'logstash'
-  mode "0400"
+  mode '0400'
 end
 
-remote_file "/etc/logstash/keys/#{node['service']['ssl_key_name']}.key" do
-  path "/etc/logstash/keys/#{node['service']['ssl_key_name']}.key"
-  source "file:///etc/ssl/private/#{node['service']['ssl_key_name']}.key"
+file "/etc/ssl/private/#{node['service']['ssl_key_name']}.key" do
   owner 'logstash'
   group 'logstash'
-  mode "0400"
+  mode '0400'
 end
 
 template "/etc/logstash/conf.d/server.conf" do
